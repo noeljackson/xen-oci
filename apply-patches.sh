4cc73c6078e9036a9531830dc88b047eb4d000b2
Alexander Merritt <alexander@edera.dev>
2026-02-11 12:34:36 -0500

    chore(build): allow apply-patches to run from elsewhere
    Signed-off-by: Alexander Merritt <alexander@edera.dev>

--- apply-patches.sh
+++ apply-patches.sh
@@ -3,19 +3,21 @@
 set -e
 
 usage() {
-	echo "usage: apply-patches.sh version component"
+	echo "usage: apply-patches.sh patchdir version component"
 	exit 1
 }
 
-[ -z "$2" ] && usage
+[ -z "$3" ] && usage
 
-patchroot="/usr/src/patches/$1/$2"
+patchroot="$1/$2/$3"
 patchseries="${patchroot}/series"
 
-[ -f "${patchseries}" ] || { echo "no patches to apply for component $2 (version $1)"; exit 0; }
+[ -f "${patchseries}" ] || {
+	echo "no patches to apply for component $3 (version $2)"
+	exit 0
+}
 
 while read -r patch; do
 	echo "Applying $patch ..."
-	patch -p1 < "${patchroot}/${patch}"
-done < ${patchseries}
-
+	patch --forward -p1 <"${patchroot}/${patch}"
+done <${patchseries}
